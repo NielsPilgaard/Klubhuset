9cd525d3eea4ccaa6b901375bbc5dd6a440be976
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2026-04-02 23:44:59 +0200

    Create migration_script.sql

---
+++ api/Skoleplanen.Api/Data/Migrations/migration_script.sql
@@ -0,0 +1,25 @@
+﻿CREATE TABLE IF NOT EXISTS "__EFMigrationsHistory" (
+    "MigrationId" character varying(150) NOT NULL,
+    "ProductVersion" character varying(32) NOT NULL,
+    CONSTRAINT "PK___EFMigrationsHistory" PRIMARY KEY ("MigrationId")
+);
+
+START TRANSACTION;
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20260402214134_InitialCreate') THEN
+    INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+    VALUES ('20260402214134_InitialCreate', '9.0.6');
+    END IF;
+END $EF$;
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20260402214433_TestMigration') THEN
+    INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+    VALUES ('20260402214433_TestMigration', '9.0.6');
+    END IF;
+END $EF$;
+COMMIT;
+
