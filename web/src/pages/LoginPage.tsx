657053f6ae5c3e045aad0c3bd92d0a2195dd7bf0
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2026-04-04 07:57:55 +0200

    Update LoginPage.tsx

--- web/src/pages/LoginPage.tsx
+++ web/src/pages/LoginPage.tsx
@@ -1,10 +1,17 @@
 import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import keycloak from '../auth/keycloak'
 
 export default function LoginPage() {
+  const navigate = useNavigate()
+
   useEffect(() => {
-    keycloak.login()
-  }, [])
+    if (keycloak.authenticated) {
+      navigate('/dashboard', { replace: true })
+    } else {
+      keycloak.login({ redirectUri: window.location.origin + '/dashboard' })
+    }
+  }, [navigate])
 
   return null
 }
