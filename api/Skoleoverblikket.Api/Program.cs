30d5347cde335a2bd87f5a34c90a4f138029699c
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2026-05-16 16:35:37 +0200

    Update Program.cs

--- api/Skoleoverblikket.Api/Program.cs
+++ api/Skoleoverblikket.Api/Program.cs
@@ -22,16 +22,16 @@ builder.Services.AddHttpContextAccessor();
 builder.Services.AddScoped<ITenantContext, HttpTenantContext>();
 builder.Services.AddMemoryCache();
 builder.Services.AddFusionCache()
-    .WithDefaultEntryOptions(o =>
-    {
-        o.Duration = TimeSpan.FromMinutes(5);
-        o.IsFailSafeEnabled = true;
-        o.FailSafeMaxDuration = TimeSpan.FromHours(1);
-        o.FailSafeThrottleDuration = TimeSpan.FromSeconds(30);
-        o.EagerRefreshThreshold = 0.9f;
-        o.FactorySoftTimeout = TimeSpan.FromSeconds(1);
-        o.FactoryHardTimeout = TimeSpan.FromSeconds(5);
-    });
+	.WithDefaultEntryOptions(o =>
+	{
+		o.Duration = TimeSpan.FromMinutes(5);
+		o.IsFailSafeEnabled = true;
+		o.FailSafeMaxDuration = TimeSpan.FromHours(1);
+		o.FailSafeThrottleDuration = TimeSpan.FromSeconds(30);
+		o.EagerRefreshThreshold = 0.9f;
+		o.FactorySoftTimeout = TimeSpan.FromSeconds(1);
+		o.FactoryHardTimeout = TimeSpan.FromSeconds(5);
+	});
 
 // Database
 builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("skoleoverblikket-db")));
